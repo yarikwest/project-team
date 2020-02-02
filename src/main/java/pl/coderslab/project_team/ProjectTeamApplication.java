982515760da9ce1cf16b432d09bdf6c49e33b3d0
yarikwest <y.zakhidnyi@gmail.com>
2020-02-02 12:41:07 +0100

    Init commit

---
+++ src/main/java/pl/coderslab/project_team/ProjectTeamApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.project_team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTeamApplication.class, args);
+    }
+
+}
