768c167696bfaeacec7077dd33a8c46384038ff1
yarikwest <y.zakhidnyi@gmail.com>
2020-02-25 18:14:18 +0100

    Add created and updated columns to all tables

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -7,7 +7,10 @@ CREATE TABLE users
     password   VARCHAR(250) NOT NULL,
     email      VARCHAR(250) NOT NULL,
     first_name VARCHAR(250),
-    last_name  VARCHAR(250)
+    last_name  VARCHAR(250),
+    created    DATETIME,
+    updated    DATETIME
+
 );
 
 DROP TABLE IF EXISTS statuses CASCADE;
@@ -17,7 +20,9 @@ CREATE TABLE statuses
     id        INT AUTO_INCREMENT PRIMARY KEY,
     name      VARCHAR(250) NOT NULL,
     level     INT          NOT NULL,
-    is_active BOOLEAN DEFAULT FALSE
+    is_active BOOLEAN DEFAULT FALSE,
+    created   DATETIME,
+    updated   DATETIME
 );
 
 DROP TABLE IF EXISTS priorities CASCADE;
@@ -27,7 +32,9 @@ CREATE TABLE priorities
     id        INT AUTO_INCREMENT PRIMARY KEY,
     name      VARCHAR(250) NOT NULL,
     color     VARCHAR(7),
-    is_active BOOLEAN DEFAULT FALSE
+    is_active BOOLEAN DEFAULT FALSE,
+    created   DATETIME,
+    updated   DATETIME
 );
 
 DROP TABLE IF EXISTS projects CASCADE;
@@ -38,8 +45,9 @@ CREATE TABLE projects
     name        VARCHAR(250) NOT NULL,
     description VARCHAR(255) NOT NULL,
     is_active   BOOLEAN DEFAULT FALSE,
+    identity    VARCHAR(250),
     created     DATETIME,
-    identity    VARCHAR(250)
+    updated     DATETIME
 );
 
 DROP TABLE IF EXISTS projects_users CASCADE;
@@ -49,6 +57,8 @@ CREATE TABLE projects_users
     id         INT AUTO_INCREMENT PRIMARY KEY,
     user_id    INT NOT NULL,
     project_id INT NOT NULL,
+    created    DATETIME,
+    updated    DATETIME,
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (project_id) REFERENCES projects (id)
 );
@@ -58,7 +68,6 @@ DROP TABLE IF EXISTS tasks CASCADE;
 CREATE TABLE tasks
 (
     id           INT AUTO_INCREMENT PRIMARY KEY,
-    created      DATETIME,
     user_id      INT,
     theme        VARCHAR(250) NOT NULL,
     description  VARCHAR(255) NOT NULL,
@@ -66,6 +75,8 @@ CREATE TABLE tasks
     status_id    INT          NOT NULL,
     priority_id  INT          NOT NULL,
     type_of_task VARCHAR(250),
+    created      DATETIME,
+    updated      DATETIME,
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (project_id) REFERENCES projects (id),
     FOREIGN KEY (status_id) REFERENCES statuses (id),
